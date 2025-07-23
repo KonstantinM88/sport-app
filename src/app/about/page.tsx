8b63fe926eb9f9843d73418eac183ca215c7e291
KonsnantinM <mihajlov8@gmail.com>
2025-07-24 00:33:07 +0200

    lesson14

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="px-28 py-20">
+      <h2>О проекте</h2>
+      <p>Платформа для онлайн-обучения различным видам спорта</p>
+    </div>
+  );
+}
