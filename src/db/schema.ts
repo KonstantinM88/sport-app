b8b6c3494b67917ede89d54743322d3d31fd994f
KonsnantinM <mihajlov8@gmail.com>
2025-07-30 09:25:27 +0200

    lesson18

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,4 +5,17 @@ export const sportsTable = pgTable("sports",{
     title: varchar({length: 255}).notNull().unique(),
     image: varchar({length: 255}).notNull(),
     description: varchar({length: 255}).notNull()
-})
+
+});
+
+export const eventsTable = pgTable("events", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }).notNull(),
+});
+
+// npx drizzle-kit push
+// npx drizzle-kit generate
+// npx drizzle-kit migrate
+// npm run migrate
+// supabase postgres
